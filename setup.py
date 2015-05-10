b2dfd8c5e152df719a6c31e3b1a198ab0d393850
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-05-10 12:00:39 +0700

    add custom setup.py test command

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ Gluu cluster management API.
 import codecs
 import os
 import re
+import sys
 from setuptools import setup
 from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
 
 
 def find_version(*file_paths):
@@ -20,6 +22,26 @@ def find_version(*file_paths):
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -34,9 +56,9 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        "Flask",
-        "Flask-RESTful",
         "flask-restful-swagger",
+        "Flask-RESTful",
+        "Flask",
         "crochet",
         "docker-py==1.1.0",
         "salt==2014.7.0",
@@ -46,10 +68,19 @@ setup(
         "netaddr",
     ],
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
+        "Framework :: Flask",
         "Intended Audience :: Developers",
+        "License :: OSI Approved",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
@@ -59,4 +90,9 @@ setup(
     entry_points={
         "console_scripts": ["gluuapi=gluuapi.cli:main"],
     },
+    tests_require=[
+        "pytest-cov",
+        "pytest",
+    ],
+    cmdclass={"test": PyTest},
 )

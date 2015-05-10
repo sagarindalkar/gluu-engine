a984a197904450319bb81e7ca44815b01db16724
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-05-10 19:23:34 +0700

    revert custom setup.py test

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ Gluu cluster management API.
 import codecs
 import os
 import re
-import sys
+# import sys
 from setuptools import setup
 from setuptools import find_packages
-from setuptools.command.test import test as TestCommand
+# from setuptools.command.test import test as TestCommand
 
 
 def find_version(*file_paths):
@@ -24,24 +24,24 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-class PyTest(TestCommand):
-    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+# class PyTest(TestCommand):
+#     user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
 
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
+#     def initialize_options(self):
+#         TestCommand.initialize_options(self)
+#         self.pytest_args = []
 
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+#     def finalize_options(self):
+#         TestCommand.finalize_options(self)
+#         self.test_args = []
+#         self.test_suite = True
 
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
+#     def run_tests(self):
+#         # import here, cause outside the eggs aren't loaded
+#         import pytest
 
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+#         errno = pytest.main(self.pytest_args)
+#         sys.exit(errno)
 
 
 setup(
@@ -90,9 +90,9 @@ setup(
     entry_points={
         "console_scripts": ["gluuapi=gluuapi.cli:main"],
     },
-    tests_require=[
-        "pytest-cov",
-        "pytest",
-    ],
-    cmdclass={"test": PyTest},
+    # tests_require=[
+    #     "pytest-cov",
+    #     "pytest",
+    # ],
+    # cmdclass={"test": PyTest},
 )

f3473633c91324eb73b3ae0972aadd4548724c0e
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-05-27 03:08:34 +0700

    a workaround to prevent AttributeError when accessing model's new attributes

--- gluuapi/database.py
+++ gluuapi/database.py
@@ -25,6 +25,7 @@ import os
 
 import jsonpickle
 import tinydb
+from werkzeug.utils import import_string
 
 
 class Database(object):
@@ -60,13 +61,24 @@ class Database(object):
             self._db = tinydb.TinyDB(self.app.config["DATABASE_URI"])
         return self._db
 
+    def _load_pyobject(self, data):
+        # ``jsonpickle.decode`` automatically instantiates object from
+        # ``py/object`` value stored in database; the problem is,
+        # this object won't recognize new attribute hence we're
+        # instantiating object and setting its attributes manually
+        cls = import_string(data["py/object"])
+        obj = cls()
+        for k, v in data.iteritems():
+            setattr(obj, k, v)
+        return obj
+
     def get(self, identifier, table_name):
         obj = None
         table = self.db.table(table_name)
         data = table.get(db.where("id") == identifier)
 
         if data:
-            obj = jsonpickle.decode(json.dumps(data))
+            obj = self._load_pyobject(data)
         return obj
 
     def persist(self, obj, table_name):
@@ -83,7 +95,7 @@ class Database(object):
     def all(self, table_name):
         table = self.db.table(table_name)
         data = table.all()
-        return [jsonpickle.decode(json.dumps(item)) for item in data]
+        return [self._load_pyobject(item) for item in data]
 
     def delete(self, identifier, table_name):
         table = self.db.table(table_name)
@@ -104,7 +116,7 @@ class Database(object):
     def search_from_table(self, table_name, condition):
         table = self.db.table(table_name)
         data = table.search(condition)
-        return [jsonpickle.decode(json.dumps(item)) for item in data]
+        return [self._load_pyobject(item) for item in data]
 
     def count_from_table(self, table_name, condition):
         table = self.db.table(table_name)

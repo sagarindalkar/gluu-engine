c971d9ac66f5e0e6bfff6bf7338126bacffa606c
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-06-09 03:06:27 +0700

    add testcase for license task

---
+++ tests/task/test_license_task.py
@@ -0,0 +1,46 @@
+import pytest
+
+@pytest.fixture
+def salt_no_cmd(monkeypatch):
+    monkeypatch.setattr(
+        "salt.client.LocalClient.cmd",
+        lambda cls, tgt, fun, arg: None,
+    )
+
+
+def test_get_expired_licenses(db, license):
+    import copy
+    from gluuapi.task import LicenseExpirationTask
+
+    # license with expired timestamp
+    db.persist(license, "licenses")
+
+    # license having ``None`` as ``expiration_date`` value
+    license2 = copy.copy(license)
+    license2.id = "abc"
+    license2.metadata["expiration_date"] = None
+    db.persist(license2, "licenses")
+
+    let = LicenseExpirationTask()
+    let._get_expired_licenses()
+
+
+def test_get_providers(db, provider, license):
+    from gluuapi.task import LicenseExpirationTask
+
+    db.persist(license, "licenses")
+    provider.license_id = license.id
+    db.persist(provider, "providers")
+
+    let = LicenseExpirationTask()
+    let._get_providers(license)
+
+
+def test_get_nodes(db, provider, oxauth_node, salt_no_cmd):
+    from gluuapi.task import LicenseExpirationTask
+
+    db.persist(provider, "providers")
+    db.persist(oxauth_node, "nodes")
+
+    let = LicenseExpirationTask()
+    let._get_nodes(provider)

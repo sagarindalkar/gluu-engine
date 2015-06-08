89846fb5e18067b97fb78712a89a18ba7a1401d5
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-06-09 02:22:33 +0700

    update testcases for License API

--- tests/resource/test_license_resource.py
+++ tests/resource/test_license_resource.py
@@ -1,6 +1,66 @@
 import codecs
 import json
 
+import pytest
+
+
+@pytest.fixture
+def oxd_resp_ok(monkeypatch):
+    class Response(object):
+        ok = True
+        text = ""
+
+        def json(self):
+            return {"license": "xyz"}
+    monkeypatch.setattr("requests.post", lambda url, data: Response())
+
+
+@pytest.fixture
+def oxd_resp_err(monkeypatch):
+    class Response(object):
+        ok = False
+        text = ""
+
+        def json(self):
+            return {"license": None}
+    monkeypatch.setattr("requests.post", lambda url, data: Response())
+
+
+@pytest.fixture
+def validator_ok(monkeypatch):
+    with codecs.open("tests/resource/validator_ok.txt", encoding="utf-8") as f:
+        patch_output = f.read()
+
+    # cannot monkeypatch ``gluuapi.utils.run`` function wrapped in
+    # ``decode_signed_license`` function,
+    # hence we're monkeypatching ``gluuapi.utils.run`` directly
+    monkeypatch.setattr(
+        "gluuapi.utils.run",
+        lambda cmd, exit_on_error: patch_output,
+    )
+
+
+@pytest.fixture
+def validator_err(monkeypatch):
+    with codecs.open("tests/resource/validator_err.txt", encoding="utf-8") as f:
+        patch_output = f.read()
+
+    # cannot monkeypatch ``gluuapi.utils.run`` function wrapped in
+    # ``decode_signed_license`` function,
+    # hence we're monkeypatching ``gluuapi.utils.run`` directly
+    monkeypatch.setattr(
+        "gluuapi.utils.run",
+        lambda cmd, exit_on_error: patch_output,
+    )
+
+
+@pytest.fixture
+def validator_invalid(monkeypatch):
+    monkeypatch.setattr(
+        "gluuapi.utils.decode_signed_license",
+        lambda sl, pk, pp, lp: {"valid": False, "metadata": None},
+    )
+
 
 def test_license_list_get(app, db, license):
     db.persist(license, "licenses")
@@ -15,29 +75,7 @@ def test_license_list_get_empty(app):
     assert json.loads(resp.data) == []
 
 
-def test_license_list_post(monkeypatch, app):
-    class Response(object):
-        ok = True
-        text = ""
-
-        def json(self):
-            return {"license": "xyz"}
-
-    # monkeypatch ``requests.post`` directly as we cannot monkeypatch
-    # ``requests`` inside ``gluuapi.utils.retrieve_signed_license`` call
-    monkeypatch.setattr("requests.post", lambda url, data: Response())
-
-    with codecs.open("tests/resource/validator_output.txt", encoding="utf-8") as fp:
-        patch_output = fp.read()
-
-    # cannot monkeypatch ``gluuapi.utils.run`` function wrapped in
-    # ``decode_signed_license`` function,
-    # hence we're monkeypatching ``gluuapi.utils.run`` directly
-    monkeypatch.setattr(
-        "gluuapi.utils.run",
-        lambda cmd, exit_on_error: patch_output,
-    )
-
+def test_license_post(app, oxd_resp_ok, validator_ok):
     resp = app.test_client().post(
         "/license",
         data={
@@ -50,18 +88,20 @@ def test_license_list_post(monkeypatch, app):
     assert resp.status_code == 201
 
 
-def test_license_list_post_notretrieved(monkeypatch, app):
-    class Response(object):
-        ok = False
-        text = ""
+def test_license_post_invalid_creds(app, oxd_resp_ok, validator_err):
+    resp = app.test_client().post(
+        "/license",
+        data={
+            "code": "abc",
+            "public_key": "pubkey a",
+            "public_password": "pubpasswd",
+            "license_password": "licensepasswd",
+        },
+    )
+    assert resp.status_code == 201
 
-        def json(self):
-            return {"license": "xyz"}
 
-    # monkeypatch ``requests.post`` directly as we cannot monkeypatch
-    # ``requests`` inside ``gluuapi.utils.retrieve_signed_license`` call
-    monkeypatch.setattr("requests.post", lambda url, data: Response())
-
+def test_license_post_notretrieved(app, oxd_resp_err):
     resp = app.test_client().post(
         "/license",
         data={
@@ -88,20 +128,8 @@ def test_license_get_notfound(app):
     assert resp.status_code == 404
 
 
-def test_license_put(monkeypatch, app, db, license):
+def test_license_put(app, db, license, validator_ok):
     db.persist(license, "licenses")
-
-    with codecs.open("tests/resource/validator_output.txt", encoding="utf-8") as fp:
-        patch_output = fp.read()
-
-    # cannot monkeypatch ``gluuapi.utils.run`` function wrapped in
-    # ``decode_signed_license`` function,
-    # hence we're monkeypatching ``gluuapi.utils.run`` directly
-    monkeypatch.setattr(
-        "gluuapi.utils.run",
-        lambda cmd, exit_on_error: patch_output,
-    )
-
     resp = app.test_client().put(
         "/license/{}".format(license.id),
         data={
@@ -125,17 +153,8 @@ def test_license_put_notfound(app):
     assert resp.status_code == 404
 
 
-def test_license_put_invalid_creds(monkeypatch, app, db, license):
+def test_license_put_invalid_creds(app, db, license, validator_err):
     db.persist(license, "licenses")
-
-    # cannot monkeypatch ``gluuapi.utils.run`` function wrapped in
-    # ``decode_signed_license`` function,
-    # hence we're monkeypatching ``gluuapi.utils.run`` directly
-    monkeypatch.setattr(
-        "gluuapi.utils.run",
-        lambda cmd, exit_on_error: "Command 'java -jar '",
-    )
-
     resp = app.test_client().put(
         "/license/{}".format(license.id),
         data={
@@ -145,3 +164,27 @@ def test_license_put_invalid_creds(monkeypatch, app, db, license):
         },
     )
     assert resp.status_code == 422
+
+
+def test_license_put_invalid_license(app, db, license, validator_invalid):
+    db.persist(license, "licenses")
+    resp = app.test_client().put(
+        "/license/{}".format(license.id),
+        data={
+            "public_key": "pubkey",
+            "public_password": "pubpasswd",
+            "license_password": "licensepasswd",
+        },
+    )
+    assert resp.status_code == 422
+
+
+def test_license_delete_notfound(app):
+    resp = app.test_client().delete("/license/abc")
+    assert resp.status_code == 404
+
+
+def test_license_delete(app, db, license):
+    db.persist(license, "licenses")
+    resp = app.test_client().delete("/license/{}".format(license.id))
+    assert resp.status_code == 204

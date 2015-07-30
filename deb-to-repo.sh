0b369edd74a138e08adcc296ca1b7c32fa191528
adrian <adrian@gluu.org>
2015-07-30 06:13:06 -0400

    fix deb-to-repo.sh

--- deb-to-repo.sh
+++ deb-to-repo.sh
@@ -4,18 +4,18 @@ trusty="/var/www/html/ubuntu/pool/main/trusty"
 trustydevel="/var/www/html/ubuntu/pool/main/trusty-devel"
 gpg_pass="/home/tomcat/.gnupg/.pass"
 build=$1
-if [ -n $build ]; then
+if [ -z $build ]; then
     build="unstable"
 fi
 if [ "$(ls -f *.deb)" ]; then
     debfile=`ls -f *.deb|sort|head -1`
     #/usr/bin/dpkg-sig -k 0544BA38 -f $gpg_pass -s builder $debfile
     ./deb-sign2.sh $debfile
-    version="trusty"
     if [ "$build" == "trusty" ]; then
         /bin/cp -f $debfile $trusty
         cd $repo_path
         `sh updaterepo-trusty.sh`
+    	version="trusty"
     else
         /bin/cp -f $debfile $trustydevel
         cd $repo_path

ad283086ed48e5aec29021179e9dbd1f01fc79cf
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-10-22 11:01:01 +0700

    add gluuapi.errors module

---
+++ gluuapi/errors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Gluu
+#
+# All rights reserved.
+
+
+class SaltEventError(Exception):
+    def __init__(self, msg, cmd_err="", exit_code=None):
+        self.msg = msg
+        self.cmd_err = cmd_err
+        self.exit_code = exit_code
+
+    def __str__(self):
+        return repr("{}: {}".format(self.msg, self.cmd_err))

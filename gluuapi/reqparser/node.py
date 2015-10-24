de0a31e3db2bd04ea983cf95b30014f5be65ace0
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-10-25 03:34:51 +0700

    refactor node reqparser

--- gluuapi/reqparser/node.py
+++ gluuapi/reqparser/node.py
@@ -69,73 +69,76 @@ class NodeReq(ma.Schema):
 
     @validates_schema
     def validate_schema(self, data):
-        self.validate_oxauth(data.get("oxauth_node_id"))
-        self.validate_saml(data.get("saml_node_id"))
+        if self.context.get("node_type") == "httpd":
+            oxauth_node_id = data.get("oxauth_node_id")
+            self.validate_oxauth(oxauth_node_id)
+
+            saml_node_id = data.get("saml_node_id")
+            if saml_node_id:
+                self.validate_saml(saml_node_id)
 
     def validate_oxauth(self, value):
-        if self.context.get("node_type") == "httpd":
-            node_in_use = db.count_from_table(
+        node_in_use = db.count_from_table(
+            "nodes",
+            db.where("oxauth_node_id") == value,
+        )
+        if node_in_use:
+            raise ValidationError("cannot reuse the oxauth node",
+                                  "oxauth_node_id")
+
+        try:
+            node = db.search_from_table(
                 "nodes",
-                db.where("oxauth_node_id") == value,
+                (db.where("id") == value) & (db.where("type") == "oxauth")
+            )[0]
+        except IndexError:
+            node = None
+
+        if not node:
+            raise ValidationError("invalid oxauth node",
+                                  "oxauth_node_id")
+
+        if node.provider_id != self.context["provider"].id:
+            raise ValidationError(
+                "only oxauth node within same provider is allowed",
+                "oxauth_node_id",
             )
-            if node_in_use:
-                raise ValidationError("cannot reuse the oxauth node",
-                                      "oxauth_node_id")
 
-            try:
-                node = db.search_from_table(
-                    "nodes",
-                    (db.where("id") == value) & (db.where("type") == "oxauth")
-                )[0]
-            except IndexError:
-                node = None
-
-            if not node:
-                raise ValidationError("invalid oxauth node",
-                                      "oxauth_node_id")
-
-            if node.provider_id != self.context["provider"].id:
-                raise ValidationError(
-                    "only oxauth node within same provider is allowed",
-                    "oxauth_node_id",
-                )
-
-            if node.state != STATE_SUCCESS:
-                raise ValidationError(
-                    "only oxauth node with SUCCESS state is allowed",
-                    "oxauth_node_id",
-                )
+        if node.state != STATE_SUCCESS:
+            raise ValidationError(
+                "only oxauth node with SUCCESS state is allowed",
+                "oxauth_node_id",
+            )
 
     def validate_saml(self, value):
-        if self.context.get("node_type") == "httpd":
-            node_in_use = db.count_from_table(
+        node_in_use = db.count_from_table(
+            "nodes",
+            db.where("saml_node_id") == value,
+        )
+        if node_in_use:
+            raise ValidationError("cannot reuse the saml node",
+                                  "saml_node_id")
+
+        try:
+            node = db.search_from_table(
                 "nodes",
-                db.where("saml_node_id") == value,
+                (db.where("id") == value) & (db.where("type") == "saml")
+            )[0]
+        except IndexError:
+            node = None
+
+        if not node:
+            raise ValidationError("invalid saml node",
+                                  "saml_node_id")
+
+        if node.provider_id != self.context["provider"].id:
+            raise ValidationError(
+                "only saml node within same provider is allowed",
+                "saml_node_id",
             )
-            if node_in_use:
-                raise ValidationError("cannot reuse the saml node",
-                                      "saml_node_id")
 
-            try:
-                node = db.search_from_table(
-                    "nodes",
-                    (db.where("id") == value) & (db.where("type") == "saml")
-                )[0]
-            except IndexError:
-                node = None
-
-            if not node:
-                raise ValidationError("invalid saml node",
-                                      "saml_node_id")
-
-            if node.provider_id != self.context["provider"].id:
-                raise ValidationError(
-                    "only saml node within same provider is allowed",
-                    "saml_node_id",
-                )
-
-            if node.state != STATE_SUCCESS:
-                raise ValidationError(
-                    "only saml node with SUCCESS state is allowed",
-                    "saml_node_id",
-                )
+        if node.state != STATE_SUCCESS:
+            raise ValidationError(
+                "only saml node with SUCCESS state is allowed",
+                "saml_node_id",
+            )

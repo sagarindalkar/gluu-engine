f426e95ff2f6974c92f1cfeb471283d4a4892b8e
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-11-02 14:26:42 +0700

    changes required for oxauth node to conform to ox v2.4.0

--- gluuapi/setup/oxauth_setup.py
+++ gluuapi/setup/oxauth_setup.py
@@ -10,30 +10,6 @@ from .base import BaseSetup
 
 
 class OxauthSetup(BaseSetup):
-    @property
-    def oxauth_errors_json(self):  # pragma: no cover
-        return self.get_template_path("nodes/oxauth/oxauth-errors.json")
-
-    @property
-    def oxauth_ldap_properties(self):  # pragma: no cover
-        return self.get_template_path("nodes/oxauth/oxauth-ldap.properties")
-
-    @property
-    def oxauth_config_xml(self):  # pragma: no cover
-        return self.get_template_path("nodes/oxauth/oxauth-config.xml")
-
-    @property
-    def oxauth_static_conf_json(self):  # pragma: no cover
-        return self.get_template_path("nodes/oxauth/oxauth-static-conf.json")
-
-    @property
-    def tomcat_server_xml(self):  # pragma: no cover
-        return self.get_template_path("nodes/oxauth/server.xml")
-
-    @property
-    def oxauth_config_marker(self):  # pragma: no cover
-        return self.get_template_path("nodes/oxauth/oxauth.config.reload")
-
     def write_salt_file(self):
         self.logger.info("writing salt file")
 
@@ -47,21 +23,16 @@ class OxauthSetup(BaseSetup):
     def gen_openid_keys(self):
         self.logger.info("generating OpenID key file")
 
-        unpack_cmd = "unzip -qq /opt/tomcat/webapps/oxauth.war " \
-                     "-d /opt/tomcat/webapps/oxauth"
-        jid = self.salt.cmd_async(self.node.id, "cmd.run", [unpack_cmd])
-        self.salt.subscribe_event(jid, self.node.id)
-
         openid_key_json_fn = os.path.join(self.node.cert_folder,
                                           "oxauth-web-keys.json")
         web_inf = "/opt/tomcat/webapps/oxauth/WEB-INF"
         classpath = ":".join([
             "{}/classes".format(web_inf),
             "{}/lib/bcprov-jdk16-1.46.jar".format(web_inf),
-            "{}/lib/oxauth-model-2.3.4.Final.jar".format(web_inf),
+            "{}/lib/oxauth-model-2.4.0.Final.jar".format(web_inf),
             "{}/lib/jettison-1.3.jar".format(web_inf),
             "{}/lib/commons-lang-2.6.jar".format(web_inf),
-            "{}/lib/log4j-1.2.14.jar".format(web_inf),
+            "{}/lib/log4j-1.2.17.jar".format(web_inf),
             "{}/lib/commons-codec-1.5.jar".format(web_inf),
         ])
         key_cmd = "java -cp {} org.xdi.oxauth.util.KeyGenerator > {}".format(
@@ -80,8 +51,8 @@ class OxauthSetup(BaseSetup):
 
     def start_tomcat(self):
         self.logger.info("starting tomcat")
-        start_cmd = "export CATALINA_PID={0}/bin/catalina.pid && " \
-                    "{0}/bin/catalina.sh start".format(self.node.tomcat_home)
+        start_cmd = "export CATALINA_PID=/var/run/tomcat.pid && " \
+                    "{}/bin/catalina.sh start".format(self.node.tomcat_home)
         jid = self.salt.cmd_async(self.node.id, "cmd.run", [start_cmd])
         self.salt.subscribe_event(jid, self.node.id)
 
@@ -130,22 +101,22 @@ class OxauthSetup(BaseSetup):
         )
 
     def render_errors_template(self):
-        src = self.oxauth_errors_json
+        src = "nodes/oxauth/oxauth-errors.json"
         dest = os.path.join(self.node.tomcat_conf_dir, os.path.basename(src))
-        self.render_template(src, dest)
+        self.render_jinja_template(src, dest)
 
     def render_config_template(self):
-        src = self.oxauth_config_xml
+        src = "nodes/oxauth/oxauth-config.json"
         dest = os.path.join(self.node.tomcat_conf_dir, os.path.basename(src))
         ctx = {
             "ox_cluster_hostname": self.cluster.ox_cluster_hostname,
-            "inumAppliance": self.cluster.inum_appliance,
-            "inumOrg": self.cluster.inum_org,
+            "inum_appliance": self.cluster.inum_appliance,
+            "inum_org": self.cluster.inum_org,
         }
-        self.render_template(src, dest, ctx)
+        self.render_jinja_template(src, dest, ctx)
 
     def render_ldap_props_template(self):
-        src = self.oxauth_ldap_properties
+        src = "nodes/oxauth/oxauth-ldap.properties"
         dest = os.path.join(self.node.tomcat_conf_dir, os.path.basename(src))
 
         ldap_hosts = ",".join([
@@ -156,33 +127,28 @@ class OxauthSetup(BaseSetup):
             "ldap_binddn": self.node.ldap_binddn,
             "encoded_ox_ldap_pw": self.cluster.encoded_ox_ldap_pw,
             "ldap_hosts": ldap_hosts,
-            "inumAppliance": self.cluster.inum_appliance,
-            "certFolder": self.node.cert_folder,
+            "inum_appliance": self.cluster.inum_appliance,
+            "cert_folder": self.node.cert_folder,
         }
-        self.render_template(src, dest, ctx)
+        self.render_jinja_template(src, dest, ctx)
 
     def render_static_conf_template(self):
-        src = self.oxauth_static_conf_json
+        src = "nodes/oxauth/oxauth-static-conf.json"
         dest = os.path.join(self.node.tomcat_conf_dir, os.path.basename(src))
         ctx = {
-            "inumOrg": self.cluster.inum_org,
+            "inum_org": self.cluster.inum_org,
         }
-        self.render_template(src, dest, ctx)
+        self.render_jinja_template(src, dest, ctx)
 
     def render_server_xml_template(self):
-        src = self.tomcat_server_xml
+        src = "nodes/oxauth/server.xml"
         dest = os.path.join(self.node.tomcat_conf_dir, os.path.basename(src))
         ctx = {
             "address": self.node.weave_ip,
-            "shibJksPass": self.cluster.decrypted_admin_pw,
-            "shibJksFn": self.cluster.shib_jks_fn,
+            "shib_jks_pass": self.cluster.decrypted_admin_pw,
+            "shib_jks_fn": self.cluster.shib_jks_fn,
         }
-        self.render_template(src, dest, ctx)
-
-    def write_marker_file(self):
-        self.logger.info("writing config marker file")
-        touch_cmd = "touch {}/oxauth.config.reload".format(self.node.tomcat_conf_dir)
-        self.salt.cmd(self.node.id, "cmd.run", [touch_cmd])
+        self.render_jinja_template(src, dest, ctx)
 
     def add_auto_startup_entry(self):
         # add supervisord entry
@@ -190,8 +156,8 @@ class OxauthSetup(BaseSetup):
         payload = """
 [program:{}]
 command={}
-environment=CATALINA_PID="{}/bin/catalina.pid"
-""".format(self.node.type, run_cmd, self.node.tomcat_home)
+environment=CATALINA_PID="/var/run/tomcat.pid"
+""".format(self.node.type, run_cmd)
 
         self.logger.info("adding supervisord entry")
         jid = self.salt.cmd_async(
@@ -212,7 +178,6 @@ environment=CATALINA_PID="{}/bin/catalina.pid"
         self.render_static_conf_template()
         self.render_server_xml_template()
         self.write_salt_file()
-        self.write_marker_file()
         self.copy_duo_creds()
         self.copy_duo_web()
         self.copy_gplus_secrets()
@@ -233,7 +198,6 @@ environment=CATALINA_PID="{}/bin/catalina.pid"
         )
 
         self.gen_openid_keys()
-        self.symlink_jython_lib()
         self.add_auto_startup_entry()
         self.start_tomcat()
         self.change_cert_access("tomcat", "tomcat")
@@ -241,12 +205,6 @@ environment=CATALINA_PID="{}/bin/catalina.pid"
 
     def teardown(self):
         self.after_teardown()
-
-    def symlink_jython_lib(self):
-        symlink_cmd = "ln -s /opt/jython/Lib " \
-                      "/opt/tomcat/webapps/oxauth/WEB-INF/lib/Lib"
-        jid = self.salt.cmd_async(self.node.id, "cmd.run", [symlink_cmd])
-        self.salt.subscribe_event(jid, self.node.id)
 
     def copy_duo_creds(self):
         src = self.get_template_path("nodes/oxauth/duo_creds.json")

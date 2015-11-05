37108ef7d03f534a01c0d8673dbdf2ad7fa71cd5
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-11-05 14:01:23 +0700

    introduce nginx node

---
+++ gluuapi/model/nginx_node.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Gluu
+#
+# All rights reserved.
+
+from .base import BaseModel
+
+
+class NginxNode(BaseModel):
+    resource_fields = dict.fromkeys([
+        "id",
+        "cluster_id",
+        "provider_id",
+        "ip",
+        "weave_ip",
+        "name",
+        "type",
+        "state",
+        "domain_name",
+    ])
+
+    def __init__(self):
+        self.id = ""
+        self.name = ""
+        self.ip = ""
+        self.weave_ip = ""
+        self.weave_prefixlen = ""
+        self.cluster_id = ""
+        self.provider_id = ""
+        self.type = "nginx"
+        self.state = ""
+        self.setup_logpath = ""
+        self.domain_name = ""
+
+        self.domain_name = ""
+        self.cert_folder = "/etc/certs"
+
+    @property
+    def recovery_priority(self):
+        return 4
---
+++ gluuapi/setup/nginx_setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Gluu
+#
+# All rights reserved.
+
+# import time
+
+from .base import BaseSetup
+
+
+class NginxSetup(BaseSetup):
+    def render_https_conf(self):
+        ctx = {
+            "ox_cluster_hostname": self.cluster.ox_cluster_hostname,
+            "cert_file": "/etc/certs/nginx.crt",
+            "key_file": "/etc/certs/nginx.key",
+            "oxauth_nodes": self.cluster.get_oxauth_objects(),
+            "oxidp_nodes": self.cluster.get_oxidp_objects(),
+        }
+
+        src = "nodes/nginx/gluu_https.conf"
+        dest = "/etc/nginx/sites-available/gluu_https.conf"
+        self.copy_rendered_jinja_template(src, dest, ctx)
+
+    def configure_vhost(self):
+        rm_cmd = "rm /etc/nginx/sites-enabled/default"
+        jid = self.salt.cmd_async(self.node.id, "cmd.run", [rm_cmd])
+        self.salt.subscribe_event(jid, self.node.id)
+
+        symlink_cmd = "ln -sf /etc/nginx/sites-available/gluu_https.conf " \
+                      "/etc/nginx/sites-enabled/gluu_https.conf"
+        jid = self.salt.cmd_async(self.node.id, "cmd.run", [symlink_cmd])
+        self.salt.subscribe_event(jid, self.node.id)
+
+    def add_auto_startup_entry(self):
+        payload = """
+[program:{}]
+command=/usr/sbin/nginx -g "daemon off;"
+""".format(self.node.type)
+        self.logger.info("adding supervisord entry")
+        jid = self.salt.cmd_async(
+            self.node.id,
+            'cmd.run',
+            ["echo '{}' >> /etc/supervisor/conf.d/supervisord.conf".format(payload)],
+        )
+        self.salt.subscribe_event(jid, self.node.id)
+
+    def restart_nginx(self):
+        self.logger.info("restarting nginx")
+        service_cmd = "supervisorctl restart nginx"
+        jid = self.salt.cmd_async(self.node.id, "cmd.run", [service_cmd])
+        self.salt.subscribe_event(jid, self.node.id)
+
+    def setup(self):
+        hostname = self.cluster.ox_cluster_hostname.split(":")[0]
+        self.gen_cert("nginx", self.cluster.decrypted_admin_pw,
+                      "www-data", "www-data", hostname)
+        self.change_cert_access("www-data", "www-data")
+        self.render_https_conf()
+        self.configure_vhost()
+        self.add_auto_startup_entry()
+        self.reload_supervisor()
+        return True

c7dacb6328ea8644789c5c37e78d67b3e1b9f668
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-11-23 12:46:29 +0700

    remove unused code

--- tests/conftest.py
+++ tests/conftest.py
@@ -218,3 +218,100 @@ def oxidp_node(cluster, provider):
     node.cluster_id = cluster.id
     node.provider_id = provider.id
     return node
+
+
+@pytest.fixture()
+def nginx_node(cluster, provider):
+    from gluuapi.model import NginxNode
+
+    node = NginxNode()
+    node.id = "nginx_{}_123".format(cluster.id)
+    node.cluster_id = cluster.id
+    node.provider_id = provider.id
+    return node
+
+
+@pytest.fixture()
+def docker_helper(request, app, provider):
+    from gluuapi.helper.docker_helper import DockerHelper
+
+    helper = DockerHelper(provider=provider)
+
+    def teardown():
+        helper.docker.close()
+
+    request.addfinalizer(teardown)
+    return helper
+
+
+@pytest.fixture(scope="session")
+def salt_helper():
+    from gluuapi.helper.salt_helper import SaltHelper
+
+    helper = SaltHelper()
+    return helper
+
+
+@pytest.fixture()
+def ldap_setup(request, app, ldap_node, cluster):
+    from gluuapi.setup import LdapSetup
+
+    setup_obj = LdapSetup(ldap_node, cluster, app)
+
+    def teardown():
+        setup_obj.remove_build_dir()
+
+    request.addfinalizer(teardown)
+    return setup_obj
+
+
+@pytest.fixture()
+def oxauth_setup(request, app, oxauth_node, cluster):
+    from gluuapi.setup import OxauthSetup
+
+    setup_obj = OxauthSetup(oxauth_node, cluster, app)
+
+    def teardown():
+        setup_obj.remove_build_dir()
+
+    request.addfinalizer(teardown)
+    return setup_obj
+
+
+@pytest.fixture()
+def oxtrust_setup(request, app, oxtrust_node, cluster):
+    from gluuapi.setup import OxtrustSetup
+
+    setup_obj = OxtrustSetup(oxtrust_node, cluster, app)
+
+    def teardown():
+        setup_obj.remove_build_dir()
+
+    request.addfinalizer(teardown)
+    return setup_obj
+
+
+@pytest.fixture()
+def httpd_setup(request, app, httpd_node, cluster):
+    from gluuapi.setup import HttpdSetup
+
+    setup_obj = HttpdSetup(httpd_node, cluster, app)
+
+    def teardown():
+        setup_obj.remove_build_dir()
+
+    request.addfinalizer(teardown)
+    return setup_obj
+
+
+@pytest.fixture()
+def oxidp_setup(request, app, oxidp_node, cluster):
+    from gluuapi.setup import OxidpSetup
+
+    setup_obj = OxidpSetup(oxidp_node, cluster, app)
+
+    def teardown():
+        setup_obj.remove_build_dir()
+
+    request.addfinalizer(teardown)
+    return setup_obj

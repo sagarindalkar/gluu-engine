a80f111c3cb05f5b5ec09659f7e7c5a7c1649b70
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-02-13 04:48:26 +0700

    update LdapSetup.gen_openid_key method

--- gluuapi/setup/ldap_setup.py
+++ gluuapi/setup/ldap_setup.py
@@ -578,8 +578,8 @@ command=/opt/opendj/bin/start-ds --quiet -N
             "commons-lang-2.6.jar",
             "log4j-1.2.17.jar",
             "commons-codec-1.5.jar",
-            "oxauth-model-2.4.1.Final.jar",
-            "oxauth-server-2.4.1.Final.jar",
+            "oxauth-model-2.4.2.Final.jar",
+            "oxauth-server-2.4.2.Final.jar",
         ])
         classpath = ":".join(jars)
         resp = self.salt.cmd(

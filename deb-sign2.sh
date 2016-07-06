229e014219bad66b916f87fc0d26e1be997d69e1
shouro <sourogts@gmail.com>
2016-07-06 14:38:35 +0600

    removing deb pkg things and adding wasi file for gunicorn

--- deb-sign2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/expect -f
-exp_internal 0
-spawn dpkg-sig -s builder -k 0544BA38 $argv
-expect "Enter passphrase: " { send "hsdif923md@5\r" }
-expect eof
---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Gluu
+#
+# All rights reserved.
+
+from gluuengine.app import create_app
+app=create_app()
+
+#to run
+#gunicorn -w $(($(nproc)*2+1)) --threads $(($(nproc)*2+1)) -k gthread -b 127.0.0.1:8080 --log-level warning --access-logfile - --error-logfile - -e API_ENV=prod,LOG_DIR=/opt/gluulog,DATA_DIR=/opt/gluudata wsgi:app

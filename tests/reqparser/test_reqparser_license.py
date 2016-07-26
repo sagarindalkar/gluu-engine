c7b08937a55f7caf8d0814a1f3d4cea20c2cee44
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-07-27 03:12:57 +0700

    fix reqparser testcase

--- tests/reqparser/test_reqparser_license.py
+++ tests/reqparser/test_reqparser_license.py
@@ -10,7 +10,6 @@ import pytest
 def test_sanitize_public_key(pubkey_in, pubkey_out):
     from gluuengine.reqparser import LicenseKeyReq
 
-    req = LicenseKeyReq().load({
-        "public_key": pubkey_in,
-    })
-    assert req.data == {"public_key": pubkey_out}
+    req = LicenseKeyReq()
+    data = req.urlsafe_public_key({"public_key": pubkey_in})
+    assert data["public_key"] == pubkey_out
--- tests/reqparser/test_reqparser_node.py
+++ tests/reqparser/test_reqparser_node.py
@@ -1,28 +1,31 @@
 import pytest
 
 
-def test_validate_invalid_provider(db):
+def test_validate_invalid_provider(app, db):
     from marshmallow import ValidationError
     from gluuengine.reqparser import NodeReq
 
-    req = NodeReq()
+    with app.app_context():
+        req = NodeReq()
 
-    with pytest.raises(ValidationError):
-        req.validate_provider("random-provider-id")
+        with pytest.raises(ValidationError):
+            req.validate_provider("random-provider-id")
 
 
-def test_validate_invalid_reused_provider(db, generic_provider, master_node):
+def test_validate_invalid_reused_provider(app, db, generic_provider,
+                                          master_node):
     from marshmallow import ValidationError
     from gluuengine.reqparser import NodeReq
 
-    master_node.provider_id = generic_provider.id
-    db.persist(generic_provider, "providers")
-    db.persist(master_node, "nodes")
+    with app.app_context():
+        master_node.provider_id = generic_provider.id
+        db.persist(generic_provider, "providers")
+        db.persist(master_node, "nodes")
 
-    req = NodeReq()
+        req = NodeReq()
 
-    with pytest.raises(ValidationError):
-        req.validate_provider(generic_provider.id)
+        with pytest.raises(ValidationError):
+            req.validate_provider(generic_provider.id)
 
 
 @pytest.mark.parametrize("name", [
@@ -39,13 +42,14 @@ def test_validate_invalid_regex_name(name):
         req.validate_name(name)
 
 
-def test_validate_reused_name(db, master_node):
+def test_validate_reused_name(app, db, master_node):
     from marshmallow import ValidationError
     from gluuengine.reqparser import NodeReq
 
-    db.persist(master_node, "nodes")
+    with app.app_context():
+        db.persist(master_node, "nodes")
 
-    req = NodeReq()
+        req = NodeReq()
 
-    with pytest.raises(ValidationError):
-        req.validate_name(master_node.name)
+        with pytest.raises(ValidationError):
+            req.validate_name(master_node.name)

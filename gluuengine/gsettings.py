fa825f9c3918b3810ad8e687842f96b4d0b87063
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-10-16 03:41:01 +0700

    initial work on launching task via server hook

--- gluuengine/gsettings.py
+++ gluuengine/gsettings.py
@@ -5,6 +5,10 @@
 
 import multiprocessing
 import os
+import time
+
+from .task import LicenseWatcherTask
+from .utils import as_boolean
 
 
 bind = ":8080"
@@ -18,10 +22,32 @@ raw_env = 'API_ENV=prod'  # 'prod|test|dev'
 
 
 def on_exit(server):
-    wsgi_app = server.app.load_wsgiapp()
-    runfile = os.path.join(wsgi_app.config["DATA_DIR"], "lwatcher.run")
+    app = server.app.load_wsgiapp()
+    runfile = os.path.join(app.config["DATA_DIR"], "lwatcher.run")
 
     try:
         os.unlink(runfile)
     except OSError:
         pass
+
+
+def post_fork(server, worker):
+    # task is launched after a worker has been forked; as task is running
+    # inside crochet/twisted reactor, we cannot use `when_ready` nor `pre_fork`
+    # hook because, somehow, reactor seems unitialized in those hooks
+    app = server.app.load_wsgiapp()
+    runfile = os.path.join(app.config["DATA_DIR"], "lwatcher.run")
+
+    if as_boolean(app.config["ENABLE_LICENSE"]):
+        if not os.path.isfile(runfile):
+            with open(runfile, "w") as fd:
+                fd.write("1")
+
+            app.logger.info("launching task on worker {}".format(worker))
+            LicenseWatcherTask(app).perform_job()
+
+
+def pre_fork(server, worker):
+    # delay before forking other workers, this will give time for a worker
+    # to claim task
+    time.sleep(0.5)

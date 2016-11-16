a4a24b14563fc34cd174fe4b84353756292abb0b
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-11-17 04:43:58 +0700

    migrate container log schema

--- gluuengine/model/log.py
+++ gluuengine/model/log.py
@@ -3,27 +3,30 @@
 #
 # All rights reserved.
 
+from schematics.types import StringType
+
 from .base import BaseModel
 from ..database import db
 
 
 class ContainerLog(BaseModel):
-    resource_fields = dict.fromkeys([
-        "id",
-        "container_name",
-        "setup_log_url",
-        "teardown_log_url",
-        "state",
-    ])
+    id = StringType()
+    container_name = StringType()
+    setup_log = StringType()
+    setup_log_url = StringType()
+    teardown_log = StringType()
+    teardown_log_url = StringType()
+    state = StringType()
 
-    def __init__(self):
-        self.id = ""
-        self.container_name = ""
-        self.setup_log = ""
-        self.setup_log_url = ""
-        self.teardown_log = ""
-        self.teardown_log_url = ""
-        self.state = ""
+    @property
+    def resource_fields(self):
+        return {
+            "id": self.id,
+            "container_name": self.container_name,
+            "setup_log_url": self.setup_log_url,
+            "teardown_log_url": self.teardown_log_url,
+            "state": self.state,
+        }
 
     @staticmethod
     def create_or_get(container):
@@ -38,7 +41,7 @@ class ContainerLog(BaseModel):
         container_log = ContainerLog()
         container_log.id = container.name
         container_log.container_name = container.name
-        container_log.setup_log = "{}-setup.log".format(container_log.container_name)
-        container_log.teardown_log = "{}-teardown.log".format(container_log.container_name)
+        container_log.setup_log = "{}-setup.log".format(container_log.container_name)  # noqa
+        container_log.teardown_log = "{}-teardown.log".format(container_log.container_name)  # noqa
         db.persist(container_log, "container_logs")
         return container_log

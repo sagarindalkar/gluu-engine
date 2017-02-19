86eac269f8cc4efa1e7b1c6539f5a6da736ce49a
Isman Firmansyah <isman.firmansyah@gmail.com>
2017-02-20 03:56:52 +0700

    remove unused code

--- gluuengine/errors.py
+++ gluuengine/errors.py
@@ -4,16 +4,6 @@
 # All rights reserved.
 
 
-class SaltEventError(Exception):
-    def __init__(self, msg, cmd_err="", exit_code=None):
-        self.msg = msg
-        self.cmd_err = cmd_err
-        self.exit_code = exit_code
-
-    def __str__(self):
-        return repr("{}: {}".format(self.msg, self.cmd_err))
-
-
 class DockerExecError(Exception):
     def __init__(self, msg, cmd_err="", exit_code=None):
         self.msg = msg
